6d8c0a6cc27f9fc19c382f6fa2ada190398b1be9
Aleksmalkov <aleksajmalkov@gmail.com>
2024-10-21 17:20:35 -0300

    vercel config change

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,20 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Automatically validate DTOs
   app.useGlobalPipes(new ValidationPipe());
 
-  app.enableCors();
-  await app.listen(5000);
+  // Enable CORS for your frontend
+  app.enableCors({
+    origin: 'https://saeki-lilac.vercel.app',  // Your frontend origin
+    credentials: true, // Allows credentials like cookies, headers
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',  // Specify allowed methods
+    allowedHeaders: 'Content-Type, Authorization',  // Allow necessary headers
+    preflightContinue: false,
+    optionsSuccessStatus: 204  // Send 204 response for successful preflight requests
+  });
+
+  await app.listen(5000);  // Start the server on port 5000
 }
 bootstrap();
