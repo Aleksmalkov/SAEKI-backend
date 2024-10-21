9db38297a98775f9bab59b0685846f6fb99ca8a2
Aleksmalkov <aleksajmalkov@gmail.com>
2024-10-21 17:54:26 -0300

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors();
-
   // Enable validation globally
   app.useGlobalPipes(new ValidationPipe());
 
   // Enable CORS globally
-  // app.enableCors({
-  //   origin: 'https://saeki-lilac.vercel.app',  // Your frontend origin
-  //   credentials: true,  // Allow credentials (cookies, etc.)
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',  // Allow necessary HTTP methods
-  //   allowedHeaders: 'Content-Type, Authorization',  // Specify allowed headers
-  // });
+  app.enableCors({
+    origin: 'https://saeki-lilac.vercel.app',  // Your frontend origin
+    credentials: true,  // Allow credentials (cookies, etc.)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',  // Allow necessary HTTP methods
+    allowedHeaders: 'Content-Type, Authorization',  // Specify allowed headers
+  });
 
   await app.listen(5000);  // Start the server on port 5000
 }
