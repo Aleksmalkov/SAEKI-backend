c372c803c10411fd4ba4fb754a7f4da5082d25d2
Aleksmalkov <aleksajmalkov@gmail.com>
2024-10-21 17:17:11 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  app.enableCors({
-    origin: 'https://saeki-lilac.vercel.app',
-    credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization',
-    preflightContinue: false,
-    optionsSuccessStatus: 204
-  });
+  app.enableCors();
   await app.listen(5000);
 }
 bootstrap();
