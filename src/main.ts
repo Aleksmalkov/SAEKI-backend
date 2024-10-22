676ebefd95d2736f746b7b2c6235e7d026edefa5
Aleksmalkov <aleksajmalkov@gmail.com>
2024-10-22 06:33:03 -0300

    change cors config

--- src/main.ts
+++ src/main.ts
@@ -9,20 +9,19 @@ async function bootstrap() {
 
   // Enable CORS globally
   app.enableCors({
-    origin: 'https://saeki-lilac.vercel.app',  // Your frontend origin
+    origin: '*',  // Your frontend origin
     credentials: true,  // Allow credentials (cookies, etc.)
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',  // Allow necessary HTTP methods
-    allowedHeaders: ['content-type'],  // Specify allowed headers
+
   });
 
-  app.use((req, res, next) => {
-    if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-      return res.status(200).end();  // Respond with a 200 for preflight requests
-    }
-    next();
-  });
+  // app.use((req, res, next) => {
+  //   if (req.method === 'OPTIONS') {
+  //     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  //     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  //     return res.status(200).end();  // Respond with a 200 for preflight requests
+  //   }
+  //   next();
+  // });
 
   await app.listen(5000);  // Start the server on port 5000
 }
