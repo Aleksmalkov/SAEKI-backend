0257f90a886ee543b904697e8c1624e1d3dcdf3b
Aleksmalkov <aleksajmalkov@gmail.com>
2024-10-22 08:00:05 -0300

    vercel config change

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   // Enable validation globally
+//   app.useGlobalPipes(new ValidationPipe());
+
+//   app.enableCors({
+//     origin: ['https://saeki-lilac.vercel.app', 'https://saeki-backend-demo.vercel.app', 'http://localhost:3000'],
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+//     credentials: true,
+//     allowedHeaders: 'Content-Type, Authorization',
+//   });
+
+//   await app.listen(5000);  // Start the server on port 5000
+// }
+// bootstrap();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // Enable validation globally
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
@@ -14,15 +32,14 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Authorization',
   });
 
-  // app.use((req, res, next) => {
-  //   if (req.method === 'OPTIONS') {
-  //     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  //     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  //     return res.status(200).end();  // Respond with a 200 for preflight requests
-  //   }
-  //   next();
-  // });
-
-  await app.listen(5000);  // Start the server on port 5000
+  await app.init();
+  
+  // Return the http adapter instance without generic type arguments
+  return app.getHttpAdapter().getInstance();
 }
-bootstrap();
+
+export const handler = async (event: any, context: any) => {
+  const server = await bootstrap();
+  const result = await server(event, context);
+  return result;
+};
